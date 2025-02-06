248710549241698233a77e3aec2250639f95e05b
Theyrus <alexpdybell@hotmail.co.uk>
2025-02-06 19:27:25 +0000

    Update Storage.sol

--- Storage.sol
+++ Storage.sol
@@ -12,5 +12,20 @@ contract Storage {
       balance = msg.value;
       owner = msg.sender;
    }
+
+   function decreaseBalance(uint amount) public returns (uint256) {
+        require(msg.sender == owner);
+        if(amount <= balance){
+          balance -= amount;  
+       }
+       return balance;
+
+    }
+    event NewPullRequest(address indexed sender, uint256 requestId);
+    
+    function createNewPR() external returns (uint256) { // Create a new PR when balance decreases!
+    require(msg.sender == Storage(owner).owner); 
+     emit NewPullRequest(msg.sender, pullRequestId);
+      return pullRequestId++;
    
 }
